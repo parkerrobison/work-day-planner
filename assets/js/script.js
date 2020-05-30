62f5d0230846527d59ce0b73b6c85ea5ddfb36bb
probison <parker.robison@hotmail.com>
2020-05-30 11:30:21 -0600

    added timers for the date up top and the timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,12 @@ var displayDate = function () {
 displayDate();
 
 
-
+var timeColor = function() {
 //using jquery we need to select all of the rows
 //this should be an array
 var timeArray = $(".text-col");
-//loop through the array
 
+//loop through the array
 // for every iteration of the array we need to know what time we are looking at
 $.each(timeArray, function (i, timeEl) {
     var hourCheck = $(timeEl).data("hour");
@@ -23,7 +23,9 @@ $.each(timeArray, function (i, timeEl) {
     console.log(currentHour);
     $(timeEl).removeClass("past present future")
 
+    // logic will decide if in the past, present, future
     if (moment(currentHour).isAfter(hourCheck)) {
+        // add the relevant class to the div
         $(timeEl).addClass("past");
     }
     else if (moment(currentHour).isBefore(hourCheck)) { 
@@ -33,7 +35,18 @@ $.each(timeArray, function (i, timeEl) {
         $(timeEl).addClass("present");
     }
 })
+}
 
+timeColor();
 
-// logic will decide if in the past, present, future
-// add the relevant class to the div
+setInterval(function () {
+    $(".text-col").each(function (tc) {
+      timeColor(tc);
+    });
+  }, 900000);
+
+setInterval(function () {
+    $("#currentDay").each(function (td) {
+        displayDate(td);
+    });
+}, 1800000);
